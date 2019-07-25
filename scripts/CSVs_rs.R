176ca61d610e8522547c6a5f2006a395af42e7cb
Isaac Brito <ibritomorales@gmail.com>
2019-07-25 09:01:42 +0200

    initial CSVs to raster idea?

---
+++ scripts/CSVs_rs.R
@@ -0,0 +1,25 @@
+rs_trial <- rasterFromXYZ(as.data.frame(new_aqm2)[, c("CenterLong", "CenterLat", "probability", "TempPrefMax", "SalinityPrefMax")]) # 0.5 deg of resolution
+plot(rs_trial)
+rs_trial <- raster::aggregate(x = rs_trial, fact = 2)
+plot(rs_trial)
+
+rs <- raster(ncol = 360, nrow = 180)
+rs[] <- 1:ncell(rs)
+up <- resample(rs_trial, rs, resample = "bilinear") # up-scaled to match the 1deg-resolution with climate models using bilenar interpolation
+plot(up)
+writeRaster(up, "rasters/new_aqm2.grd", overwrite = TRUE)
+
+new_up <- as.data.frame(rasterToPoints(up)) # 1 degree spatial resolution
+# plot(boundaries(rs_trial, type = "inner")) # inner boundaries for species distribution edges
+
+wb <- "/Users/bri273/Dropbox/02_/Chapter02/ShapeFiles/WorldBorders/"
+wb <- st_read(wb)
+wb_sp <- as(wb, "Spatial")
+
+graphics.off()
+pdf(file = "figs/SalinityPrefMax_sp1.pdf", width = 38, height = 20)
+plot(up$SalinityPrefMax)
+plot(wb_sp, add = TRUE, col = "gray54")
+dev.off()
+# species ID to math with hcaf distributions and extract depth (min max mean) and thermal + pH limits
+
