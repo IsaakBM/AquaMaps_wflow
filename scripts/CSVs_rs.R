fda26db9c379d368b148675c45352536a77004df
Isaac Brito <ibritomorales@gmail.com>
2019-08-05 13:32:34 +1000

    almost (ex. fixing NULL element list)

--- scripts/CSVs_rs.R
+++ scripts/CSVs_rs.R
@@ -11,16 +11,17 @@
 # bathymetry_shp: a bathymetry shapefile to constrain species cells (ETOPO dataset)
 
 # Input Files
-# 1. species information (e.g. species ID, taxonomy, "reviewed" or not, but without spatial information)  = hcaf.csv
-# 2. information for each half-degree cell (e.g. max/min depth, salinity, temperature, etc) = hpen.csv
-# 3. a lookup table of which species are found in each half-degree cell (and the "probability of occurrence" of that species in that cell)   = cell.csv
+# 1. 
+# 2. 
+# 3. 
 
-aqua_rs <- function(path, resolution, bathymetry_shp) { # kill the cells that are not according with the bathymetry... 
+aqua_rs <- function(path, bathymetry_shp, olayer) { # kill the cells that are not according with the bathymetry... 
   
   library(dplyr)
   library(raster)
   library(sf)
-  
+  library(foreach)
+  library(doParallel)
   
   # .csv files by species
   files_csv <- list.files(path = path, pattern = ".csv", full.names = TRUE) 
@@ -32,33 +33,39 @@ aqua_rs <- function(path, resolution, bathymetry_shp) { # kill the cells that ar
   registerDoParallel(cl)
     # Loops to create rasters (0.5 degree res)
     rs_list <- foreach(i = 1:length(files_csv), .packages = c("dplyr", "raster")) %dopar% {
-      single <- read.csv(path[i])
-      rs_list[[i]] <- rasterFromXYZ(as.data.frame(single)
-                                    [, c("CenterLong", "CenterLat", "probability", "TempPrefMin","TempPrefMax", "SalinityPrefMin","SalinityPrefMax")])  
+      single <- read.csv(files_csv[i])
+      if(nrow(single) > 1) { # some species have only 1 cell (avoid them)
+        rs_list[[i]] <- rasterFromXYZ(as.data.frame(single)[, c("CenterLong", "CenterLat", "probability", "TempPrefMin","TempPrefMax", "SalinityPrefMin","SalinityPrefMax")])   
+      }
     } 
   
   # 2. ETOPO database overlap with distribution: 
   # match NA cells with current dsitribution cells of species in order to get an uniform representation of layers  
   # Bathymetry layers
     bathy <- raster(bathymetry_shp)
-      bathy[] <- ifelse(bathy[] > 0, NA, bathy[]) # keeping only "negative" value (i.e. depths)
+      bathy[] <- ifelse(bathy[] > 0, NA, bathy[]) # keeping only "negative" value (i.e. depths) <- <- ? to make a copy?
      if (olayer == "surface") {
-      bathy_layer[] <- ifelse(bathy[] > 0, NA, bathy[])
+      bathy[] <- ifelse(bathy[] > 0, NA, bathy[])
     } else if (olayer == "mesopelagic") {
-      bathy_layer[] <- ifelse(bathy[] > -200, NA, bathy[])
+      bathy[] <- ifelse(bathy[] > -200, NA, bathy[])
     } else if (olayer == "bathypelagic") {
-      bathy_layer[] <- ifelse(bathy[] > -1000, NA, bathy[])
+      bathy[] <- ifelse(bathy[] > -1000, NA, bathy[])
     } else if (olayer == "abyssopelagic") {
-      bathy_layer[] <- ifelse(bathy[] > -4000, NA, bathy[])
+      bathy[] <- ifelse(bathy[] > -4000, NA, bathy[])
     } else {
-      bathy_layer[] <- ifelse(bathy[] > 0, NA, bathy[])
+      bathy[] <- ifelse(bathy[] > 0, NA, bathy[])
     }
   
   # 3. Match Bathymetry and species raster distributions spatial resolution
   rs_final <- vector("list", length(rs_list))
   for (j in 1:length(rs_list)) {
-    single <- subset(rs_list[[j]])    
-    rs_final[[j]] <- mask(rs_list, resample(bathy_layer, rs_list, resample = "bilinear"))
+    # single <- subset(rs_list[[j]])    
+    rs_final[[j]] <- mask(rs_list[[j]], resample(bathy, rs_list[[j]], resample = "bilinear"))
+    print(paste0(j, " of ", length(rs_list)))
+    
+    # name.csv <- paste(IDs_df[[j]][1,1], olayer, sep = "_")
+    # writeRaster(IDs_df[[j]], paste(outdir, name.csv, ".csv", sep = ""), row.names = FALSE)
+    # print(paste0(j, " of ", length(IDs_df)))
   }
   return(rs_final)
 }
