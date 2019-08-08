d09e7ad963cc7cdae74f37819db1aaab09a32907
Isaac Brito <ibritomorales@gmail.com>
2019-08-08 13:27:17 +1000

    cleaning

--- scripts/UsingAQMFx.R
+++ scripts/UsingAQMFx.R
@@ -1,60 +1,12 @@
 source("scripts/messingAQM.R")
-trial <- aqua_start(path = "AquaMaps/", outdir = "CSVs/", olayer = "abyssopelagic", prob_threshold = 0.4, data = "species")
-trial2 <- aqua_rs(path = "CSVs", bathymetry_shp = "shapefiles/ETOPO1/ETOPO1_ocean.grd", olayer = "abyssopelagic")
-
-
-sp_richness <- do.call(rbind, trial)
-sp_richness <- sp_richness %>% group_by(CenterLat, CenterLong) %>% summarise(richness = n()) %>% data.frame()
-head(sp_richness)
-dim(sp_richness)
-
-single <- read.csv("CSVs/Fis-139258_abyssopelagic.csv")
-rs_trial <- rasterFromXYZ(as.data.frame(single)[, c("CenterLong", "CenterLat", "probability")]) # 0.5 deg of resolution
-namers <- raster(ncol = 720, nrow = 360) #720:360...0.5 degree res
-rs[] <- 1:ncell(n)
-trial <- resample(rs_trial, rs, resample = "bilinear")
-plot(rs_trial)
-plot(trial)
-# plot(rs_trial$richness)
-# rs_trial <- raster::aggregate(x = rs, fact = 2)
-# plot(rs_trial)
-files_csv <- list.files(path = "CSVs", pattern = ".csv", full.names = TRUE) 
-paste(read.csv(files_csv[1])[1,1])
-
-
-wb <- "/Users/bri273/Dropbox/02_/Chapter02/ShapeFiles/WorldBorders/"
-wb <- st_read(wb)
-wb_sp <- as(wb, "Spatial")
-
-graphics.off()
-pdf(file = "figs/abyssopelagic_06.pdf", width = 38, height = 20)
-plot(trial2[[6]]$probability)
-plot(wb_sp, add = TRUE, col = "gray54")
-dev.off()
-
-trial
-for(j in 1:length(trial)) {
-  name.csv <- paste(trial[[j]][1,1], sep = "_")
-  write.csv(trial[[j]], paste("CSVs/", name.csv,".csv", sep = ""), row.names = FALSE)
-  print(paste0(j, " of ", length(trial)))
-  # return(IDs_df)
-}
-
-
-trial2
-outdir = "rasters"
-  for(k in 1:length(trial2)) {
-    name.rs <- paste(read.csv(files_csv[k])[1,1], "abyssopelagic", sep = "_")
-    writeRaster(trial2[[k]], paste(outdir, name.rs, ".grd", sep = ""), overwrite = TRUE)
-    print(paste0(j, " of ", length(trial2)))
-    }
-name.rs <- paste(read.csv(files_csv[1])[1,1], "abyssopelagic", sep = "_")
-writeRaster(trial2[[1]], paste(outdir, name.rs, ".grd", sep = ""), overwrite = TRUE)
-writeRaster(readAll(trial2[[1]]), "rasters/trial.grd")
-
+abysso_spp_csv <- aqua_start(path = "AquaMaps/", outdir = "CSVs/", olayer = "abyssopelagic", prob_threshold = 0.4, data = "species")
+abysso_spp_rs <- aqua_rs(path = "CSVs", bathymetry_shp = "shapefiles/ETOPO1/ETOPO1_ocean.grd", olayer = "abyssopelagic")
 
 
 # plotting each file as a PDF
+  wb <- "/Users/bri273/Dropbox/02_/Chapter02/ShapeFiles/WorldBorders/"
+  wb <- st_read(wb)
+  wb_sp <- as(wb, "Spatial")
   for(i in 1:length(trial2)) {
     if(length(trial2[[i]]) != 0) {
       graphics.off()
