c72418de66be68554a250355a3b684d637ce119b
Isaac Brito <ibritomorales@gmail.com>
2019-09-18 15:29:27 +1000

    update generic for hpc

--- scripts/UsingFxs_PBS.R
+++ scripts/UsingFxs_PBS.R
@@ -1,7 +1,7 @@
-source("/Users/bri273/Desktop/AquaMaps_wflow/scripts/messingAQM.R")
+source("/30days/uqibrito/AquaMaps_wflow/scripts/messingAQM.R")
 
-abysso_spp_csv <- aqua_start(path = "/Users/bri273/Desktop/AquaMaps_wflow/AquaMaps", 
-                             outdir = "/Users/bri273/Desktop/AquaMaps_wflow/CSVs/", 
+abysso_spp_csv <- aqua_start(path = "/30days/uqibrito/AquaMaps_wflow/AquaMaps", 
+                             outdir = "/30days/uqibrito/AquaMaps_wflow/CSVs/", 
                              olayer = "abyssopelagic", prob_threshold = 0.4,
                              data = "species", 
-                             region = "/Users/bri273/Desktop/AquaMaps_wflow/ETOPO1_05deg/ETOPO1_ocean.grd")
+                             region = "/30days/uqibrito/AquaMaps_wflow/ETOPO1_05deg/ETOPO1_ocean.grd")
