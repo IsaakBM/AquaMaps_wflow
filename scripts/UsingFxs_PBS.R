6b5803f0fc69f63bb1ed9cce008104fb4a6a239d
Isaac Brito <ibritomorales@gmail.com>
2019-09-19 13:47:55 +1000

    delete

--- scripts/UsingFxs_PBS.R
+++
@@ -1,7 +0,0 @@
-source("/30days/uqibrito/AquaMaps_wflow/scripts/messingAQM.R")
-
-abysso_spp_csv <- aqua_start(path = "/30days/uqibrito/AquaMaps_wflow/AquaMaps", 
-                             outdir = "/30days/uqibrito/AquaMaps_wflow/CSVs/", 
-                             olayer = "abyssopelagic", prob_threshold = 0.4,
-                             data = "species", 
-                             region = "/30days/uqibrito/AquaMaps_wflow/ETOPO1_05deg/ETOPO1_ocean.grd")
