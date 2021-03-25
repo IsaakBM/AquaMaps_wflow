4fdd59f36824c6ea17f4d1beb8df4fb4be58bd3a
Isaac Brito <ibritomorales@gmail.com>
2021-03-25 16:01:36 +1000

    adding fig and update commit

--- scripts/01a_RunAQM-RDS_v2019.R
+++ scripts/01a_RunAQM-RDS_v2019.R
@@ -16,3 +16,39 @@ test01 <- aqua_start(path = "AquaMaps/v2019a",
                      sp_env = 1,
                      type = "Pacific",
                      region = "InputFiles/PacificCentred_05deg/PacificCentred_05deg.tif")
+
+
+####################################################################################
+####### Plotting species richness
+####################################################################################
+library(RColorBrewer)
+library(ggplot2)
+library(sf)
+
+dt <- readRDS("inp")
+final <- dt %>%
+  dplyr::mutate(richness_log = log10(richness))
+  dplyr::mutate(rich_categ = ifelse(richness_log == 0, 1,
+                             ifelse(richness_log > 0 & richness_log <= 1, 2,
+                             ifelse(richness_log > 1 & richness_log <= 1.69897, 3,
+                             ifelse(richness_log > 1.69897 & richness_log <= 2, 4, 
+                             ifelse(richness_log > 2 & richness_log <= 2.69897, 5, 
+                             ifelse(richness_log > 2.69897 & richness_log <= 3, 6, 7)))))))
+
+# Defining generalities
+pal_rich <- rev(brewer.pal(7, "RdYlBu"))
+cv_rich <- c("1", "1 - 10", "10 - 50", "50 - 100", "100 - 500", "500 - 1000", "> 1000")
+world_sf <- readRDS("InputFiles/WorldPacificCentred/WorldPacificCentred.rds")
+# Plotting the figures
+p <- ggplot() +
+  geom_sf(data = final, aes(fill = rich_categ), color = NA) +
+  geom_sf(data = world_sf, size = 0.05, fill = "grey20") +
+  scale_fill_gradientn(name = "Richness",
+                       colours = pal_rich,
+                       limits = c(1, 7),
+                       breaks = seq(1, 7, 1),
+                       labels = cv_rich) +
+  ggsave("Figs/PacificRichness_01.pdf", width = 20, height = 10, dpi = 300)
+
+
+
